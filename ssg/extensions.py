b8d25bce21977495404957249154cc77ab07265f
James F. Campanella <jamessc@siu.edu>
2021-03-26 10:43:19 -0500

    Passed 3 test in Module1 failing on 3.

---
+++ ssg/extensions.py
@@ -0,0 +1,13 @@
+import sys
+import importlib
+from pathlib import Path
+
+def load_module(directory, name):
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+
+def load_directory(directory):
+    for path in directory:
+        load_module(directory.as_posix(), (path.rglob("*.py")).stem)
