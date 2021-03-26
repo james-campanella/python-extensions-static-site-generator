0d2d7141955493ce359fd320d86f0acf323304c7
James F. Campanella <jamessc@siu.edu>
2021-03-26 15:56:49 -0500

    Trying to get 4th test to pass.

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -7,3 +7,7 @@ def register(hook, order=0):
     return register_callback
 
 
+def event(hook, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
