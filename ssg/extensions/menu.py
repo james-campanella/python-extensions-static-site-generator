7846f94998cfc37b2c7b90fe6ef0e3d7cdf43a88
James F. Campanella <jamessc@siu.edu>
2021-03-26 17:40:27 -0500

    Passed test 6 of module 4.

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -16,3 +16,6 @@ def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext: template.format(name.extension(ext), name)
     menu = "\n".join([menu_item(path.stem, ext) for path in files])
+    return "<ul>\n{}</ul>\n{}".format(menu, html)
+
+
